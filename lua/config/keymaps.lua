40f0c0dd981efaca09af10bf5109d8b383c77be2
minortex <texsd.tt29@outlook.com>
2026-03-15 17:47:38 +0800

    use F5 to insert time

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,9 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
 -- 将插入模式下的 Ctrl+c 映射为 Esc，以确保能正确触发 InsertLeave 等事件
 vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode properly" })
+
+-- insert time quickly via F5
+vim.keymap.set("n", "<F5>", function()
+  local timestamp = vim.fn.strftime("%Y-%m-%dT%H:%M:%S+08:00")
+  vim.api.nvim_put({ timestamp }, "", false, true)
+end, { noremap = true, silent = true, desc = "Insert ISO Timestamp" })
