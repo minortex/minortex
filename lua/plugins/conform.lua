2e8403b8534c74f3ef95b7c41a7e8aa311020438
minortex <texsd.tt29@outlook.com>
2026-03-17 23:39:02 +0800

    feat: replace pangu with autocorrect

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,12 +6,13 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      markdown = { "pangu" }, -- 这样保存时就会自动执行 pangu 格式化
+      markdown = { "autocorrect" },
     },
     formatters = {
-      pangu = {
-        command = "pangu",
-        args = { "-" }, -- 从标准输入读取并输出
+      autocorrect = {
+        command = "autocorrect",
+        args = { "autocorrect", "--fix", "--stdin", "--quiet" },
+        stdin = true,
       },
     },
   },
