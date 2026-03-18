2f575f9bbd8334419e9f3507167848db7a079542
minortex <texsd.tt29@outlook.com>
2026-03-18 17:52:25 +0800

    chore: add autocorrect installation guide

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,12 @@
---- 使用 pangu 来进行格式化
---- 先执行：
---- uvx pangu
---- uv tool install pangu
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
       markdown = { "autocorrect" },
     },
+    --- 使用 autocorrect 来进行格式化
+    --- 先执行：
+    --- bun add -g autocorrect-node
     formatters = {
       autocorrect = {
         command = "autocorrect",
