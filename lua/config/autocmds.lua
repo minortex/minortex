5ef389760aad648f6deecd76950cc2111c15eb3a
minortex <texsd.tt29@outlook.com>
2026-04-01 23:27:00 +0800

    chore: optimize joplin's external editor behavior

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*/joplin-desktop/*.md", -- 匹配 Joplin 的临时目录
+  callback = function()
+    vim.opt_local.backup = false
+    vim.opt_local.writebackup = false
+    vim.opt_local.backupcopy = "yes"
+    -- 顺便可以开启自动保存，体验更好
+    -- vim.opt_local.autowriteall = true
+  end,
+})
