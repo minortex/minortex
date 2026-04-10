52309e3cec2689ead19d545568155d7c437cbf03
minortex <texsd.tt29@outlook.com>
2026-04-10 22:22:09 +0800

    chore: force tokyonight to use light theme

--- lua/plugins/jieba.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kkew3/jieba.vim",
-  tag = "v1.0.5",
-  build = "./build.sh",
-  init = function()
-    vim.g.jieba_vim_lazy = 1
-    vim.g.jieba_vim_keymap = 1
-  end,
-}
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,6 +3,7 @@ return {
     "tokyonight.nvim",
     opts = {
       transparent = true,
+      style = "day",
       styles = {
         sidebars = "transparent",
         floats = "transparent",
